7304fa6bebc2db89e55bf8f2c94e21e278cc8ab5
Juan <juanpiromanok@gmail.com>
2026-09-13 20:38:03 -0300

    formato correcto

--- src/comandos.c
+++ src/comandos.c
@@ -10,136 +10,107 @@ char rareza_a_letra(enum rareza_pokemon rareza);
 
 void imprimir_pokemon(const struct pokemon *pokemon)
 {
-    if (pokemon == NULL) {
-        return;
-    }
+	if (pokemon == NULL) {
+		return;
+	}
 
-    printf(
-        "%s,%d,%g,%c\n",
-        pokemon->nombre,
-        pokemon->velocidad,
-        pokemon->peso,
-        rareza_a_letra(pokemon->rareza)
-    );
+	printf("%s,%d,%g,%c\n", pokemon->nombre, pokemon->velocidad,
+	       pokemon->peso, rareza_a_letra(pokemon->rareza));
 }
 
-bool imprimir_todos(
-    struct pokemon *pokemon,
-    void *extra
-)
+bool imprimir_todos(struct pokemon *pokemon, void *extra)
 {
-    (void)extra;
-    imprimir_pokemon(pokemon);
-    return true;
+	(void)extra;
+	imprimir_pokemon(pokemon);
+	return true;
 }
 
 struct filtro_rareza {
-    enum rareza_pokemon rareza;
+	enum rareza_pokemon rareza;
 };
 
-bool imprimir_si_coincide_rareza(
-    struct pokemon *pokemon,
-    void *extra
-)
+bool imprimir_si_coincide_rareza(struct pokemon *pokemon, void *extra)
 {
-    struct filtro_rareza *filtro = extra;
+	struct filtro_rareza *filtro = extra;
 
-    if (pokemon == NULL || filtro == NULL) {
-        return false;
-    }
+	if (pokemon == NULL || filtro == NULL) {
+		return false;
+	}
 
-    if (pokemon->rareza == filtro->rareza) {
-        imprimir_pokemon(pokemon);
-    }
+	if (pokemon->rareza == filtro->rareza) {
+		imprimir_pokemon(pokemon);
+	}
 
-    return true;
+	return true;
 }
 
 int comando_buscar_nombre(tp1_t *tp1, const char *nombre)
 {
-    if (tp1 == NULL || nombre == NULL) {
-        return 1;
-    }
+	if (tp1 == NULL || nombre == NULL) {
+		return 1;
+	}
 
-    struct pokemon *pokemon =
-        tp1_buscar_pokemon(tp1, nombre);
+	struct pokemon *pokemon = tp1_buscar_pokemon(tp1, nombre);
 
-    if (pokemon == NULL) {
-        fprintf(
-            stderr,
-            "No se encontro el pokemon %s\n",
-            nombre
-        );
+	if (pokemon == NULL) {
+		fprintf(stderr, "No se encontro el pokemon %s\n", nombre);
 
-        return 1;
-    }
+		return 1;
+	}
 
-    imprimir_pokemon(pokemon);
-    return 0;
+	imprimir_pokemon(pokemon);
+	return 0;
 }
 
 int comando_mostrar_uno(tp1_t *tp1)
 {
-    size_t cantidad = tp1_cantidad(tp1);
+	size_t cantidad = tp1_cantidad(tp1);
 
-    if (cantidad == 0) {
-        fprintf(stderr, "La pokedex esta vacia\n");
-        return 1;
-    }
+	if (cantidad == 0) {
+		fprintf(stderr, "La pokedex esta vacia\n");
+		return 1;
+	}
 
-    srand((unsigned int)time(NULL));
+	srand((unsigned int)time(NULL));
 
-    size_t posicion = (size_t)rand() % cantidad;
+	size_t posicion = (size_t)rand() % cantidad;
 
-    struct pokemon *pokemon =
-        tp1_buscar_orden(tp1, posicion);
+	struct pokemon *pokemon = tp1_buscar_orden(tp1, posicion);
 
-    if (pokemon == NULL) {
-        return 1;
-    }
+	if (pokemon == NULL) {
+		return 1;
+	}
 
-    imprimir_pokemon(pokemon);
-    return 0;
+	imprimir_pokemon(pokemon);
+	return 0;
 }
 
 int comando_mostrar_todo(tp1_t *tp1)
 {
-    if (tp1 == NULL) {
-        return 1;
-    }
+	if (tp1 == NULL) {
+		return 1;
+	}
 
-    tp1_iterar(tp1, imprimir_todos, NULL);
-    return 0;
+	tp1_iterar(tp1, imprimir_todos, NULL);
+	return 0;
 }
 
 int comando_mostrar_rarezas(tp1_t *tp1)
 {
-    if (tp1 == NULL) {
-        return 1;
-    }
+	if (tp1 == NULL) {
+		return 1;
+	}
 
-    struct filtro_rareza filtro;
+	struct filtro_rareza filtro;
 
-    filtro.rareza = RAREZA_COMUN;
-    tp1_iterar(
-        tp1,
-        imprimir_si_coincide_rareza,
-        &filtro
-    );
+	filtro.rareza = RAREZA_COMUN;
+	tp1_iterar(tp1, imprimir_si_coincide_rareza, &filtro);
 
-    filtro.rareza = RAREZA_RARO;
-    tp1_iterar(
-        tp1,
-        imprimir_si_coincide_rareza,
-        &filtro
-    );
+	filtro.rareza = RAREZA_RARO;
+	tp1_iterar(tp1, imprimir_si_coincide_rareza, &filtro);
 
-    filtro.rareza = RAREZA_LEGENDARIO;
-    tp1_iterar(
-        tp1,
-        imprimir_si_coincide_rareza,
-        &filtro
-    );
+	filtro.rareza = RAREZA_LEGENDARIO;
+	tp1_iterar(tp1, imprimir_si_coincide_rareza, &filtro);
 
-    return 0;
+	return 0;
 }
